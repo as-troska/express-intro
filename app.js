e965156be5cdc16502af44e5d3b265ddaaa3e9db
Trond Sneås Skauge <troska@asvg.no>
2024-01-08 13:59:16 +0100

    Lagt til fetch og send

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 
+let navn = "Dole"
+
 
 app.get("/info", (req, res) => {
     console.log("Noen prøvde å nå /info")
@@ -13,10 +15,18 @@ app.get("/info", (req, res) => {
     res.send("OK")
 });
 
+app.get("/sendData", (req, res) => {
+    res.send(navn)
+})
+
 app.post("/infoPost", (req, res) => {
     console.log("Noen prøvde å nå /info")
     console.log(req.body)
-    res.send("OK")
+
+    navn = req.body.navn
+
+    res.sendFile(path.join(__dirname, "public", "taimot.html"))
+
 });
 
 app.listen(3000, () => {
