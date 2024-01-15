67d5dc5e4407397a5a7cb4134c7b83e534aa06ac
Trond Sneås Skauge <troska@asvg.no>
2024-01-15 12:49:25 +0100

    Enkelt insert og select-eksempel

--- app.js
+++ app.js
@@ -1,34 +1,44 @@
+//Importerer pakkene vi skal bruke
 const express = require('express');
 const path = require('path');
+const db = require("better-sqlite3")("datbase.db");
 
+//Lager en instans av Express i variabelen app
 const app = express();
 
+//Setter opp en mappe for statiske filer
 app.use(express.static(path.join(__dirname, "public")));
+
+//Gjør at vi kan lese data fra et skjema som sendes inn via POST
 app.use(express.urlencoded({ extended: false }));
 
-let navn = "Dole"
-
-
-app.get("/info", (req, res) => {
-    console.log("Noen prøvde å nå /info")
-    console.log(req.query)
-    res.send("OK")
+//Setter opp en rute som kan ta imot et skjema som sendes inn via POST
+app.post("/registrer", (req, res) => {
+    //Lager en SQL-setning som setter inn data i databasen når den kjøres. Spørsmålstegnene representerer verdier som skal settes inn.
+    const stmt = db.prepare("INSERT INTO user (fornavn, etternavn, fdato, epost, brukernavn, passord) VALUES (?, ?, ?, ?, ?, ?)");
+    //Kjører SQL-setningen. Verdiene fra skjemaet settes inn på plassene til spørsmålstegnene.
+    stmt.run(req.body.fornavn, req.body.etternavn, req.body.fdato, req.body.epost, req.body.brukernavn, req.body.passord);
+    //Sender brukeren til velkommen.html
+    res.sendFile(path.join(__dirname, "public", "velkommen.html"))
 });
 
-app.get("/sendData", (req, res) => {
-    res.send(navn)
-})
-
-app.post("/infoPost", (req, res) => {
-    console.log("Noen prøvde å nå /info")
-    console.log(req.body)
-
-    navn = req.body.navn
-
-    res.sendFile(path.join(__dirname, "public", "taimot.html"))
-
+app.post("/login", (req, res) => {
+    //Lager en SQL-setning som henter ut data fra databasen når den kjøres. Spørsmålstegnene representerer verdier som skal hentes ut.
+    const stmt = db.prepare("SELECT * FROM user WHERE brukernavn = ?");
+    //Kjører SQL-setningen. Verdiene fra skjemaet settes inn på plassene til spørsmålstegnene. Det lagres et objekt med all informasjonen i variabelen user.
+    const user = stmt.get(req.body.brukernavn);
+    
+    //Sjekker om brukeren finnes
+    if (user && user.passord === req.body.passord) {
+        //Sender brukeren til velkommen.html
+        res.sendFile(path.join(__dirname, "public", "velkommen.html"))
+    } else {
+        //Sender brukeren til feil.html
+        res.sendFile(path.join(__dirname, "public", "feil.html"))
+    }
 });
 
+//Starter sørveren på port 3000
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
